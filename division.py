8028bce2560baba0b8284ff79a0a84d7119dd1ee
asmagina1995 <asmagina1995@users.noreply.github.com>
2018-05-30 08:54:09 +0000

    Update division.py

--- division.py
+++ division.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
     try:
-        print(args.numerator / args.divisor)
+        print(args.numeratorawv7ie72367qwgasdouasdfasffaFS / args.divisor)
     except:
         raise io.RumTimeError
